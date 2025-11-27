5ad9f34a9f5981455ac7993515218a75e9747133
Riya Warikoo <121093317+RiyaWarikoo@users.noreply.github.com>
2025-11-27 15:00:29 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,8 +1,2 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-num = 5
-print(f"The factorial of {num} is {factorial(num)}")
+def test_dummy():
+    assert True
